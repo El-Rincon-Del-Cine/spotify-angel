4fe58ba7817c881a7ee447870258f1354072932a
Angel Mauricio <168794154+El-Rincon-Del-Cine@users.noreply.github.com>
2025-03-29 16:18:57 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('../sw.js')
+            .then((registration) => {
+                console.log('Service Worker registrado', registration.scope);
+
+                // Solicita permiso para las notificaciones al cargar la página
+                if (Notification.permission === "default") {
+                    Notification.requestPermission().then(permission => {
+                        if (permission === "granted") {
+                            registration.active?.postMessage({ type: 'SHOW_NOTIFICATION' });
+                        }
+                    });
+                } else if (Notification.permission === "granted") {
+                    registration.active?.postMessage({ type: 'SHOW_NOTIFICATION' });
+                }
+            })
+            .catch((error) => {
+                console.log('Error al registrar el Service Worker:', error);
+            });
+    });
+}
+
 const API_KEY = "4e31a0b780mshc91bb9f90dd6fadp1dd141jsncba4ecac40cb";
 const API_HOST = "spotify23.p.rapidapi.com";
 
